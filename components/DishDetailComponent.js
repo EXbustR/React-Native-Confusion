f4fdf95178fe075e441911485db3eb2219a5b0a5
Jose Alcaraz <jalcaraz007@gmail.com>
2019-02-23 11:50:04 -0800

    Navigation Part 2

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { View, FlatList } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
 
 function RenderDish(props) {
     const dish = props.dish;
@@ -23,11 +24,30 @@ function RenderDish(props) {
     }
 }
 
-function Dishdetail(props) {
-    return(<RenderDish dish={props.dish} />);
+
+
+class DishDetail extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
+
+    static NavigationOptions = {
+        title: 'Dish Details'
+    };
+
+    render() {
+        const dishId = this.props.navigation.getParam('dishId', '');
+
+        return(<RenderDish dish={this.state.dishes[+dishId]} />);
+
+    }
 }
 
 
 
-export default Dishdetail;
+export default DishDetail;
 
